1c43332626a1c8e6033c46a7002c7f2b9e089d2d
kotebeg <kotebeg@gmail.com>
2025-08-18 01:29:30 +0400

    correct css errors

--- labProjcets/settings.py
+++ labProjcets/settings.py
@@ -23,12 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!zyu28_=$g#hlrfr)(p*1ty(%3@1j#p0@tkmzh6b0x0zz#*@6w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True   # false
+DEBUG = False   # True False
 
-ALLOWED_HOSTS = ["kotebeg.pythonanywhere.com"
-                "127.0.0.1",    #to be deleted
-                "localhost",]   #to be deleted
+# ALLOWED_HOSTS = ["kotebeg.pythonanywhere.com",
+#                 "127.0.0.1",    #to be deleted
+#                 "localhost",]   #to be deleted
 
+
+ALLOWED_HOSTS = ["kotebeg.pythonanywhere.com",]
 
 # Application definition
 
